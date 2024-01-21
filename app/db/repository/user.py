72c5ab3275abe59fc218de3c01e0948a9d4353db
alexexisted <a.assjones@list.ru>
2024-01-21 18:09:49 +0300

    added function with db query to show user's information

--- app/db/repository/user.py
+++ app/db/repository/user.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import Session
-from schemas.user import UserCreate
+from schemas.user import UserCreate, ShowUser
 from db.models.user import User
 from core.hashing import Hasher
 
 """
+Here locate all functions which connected to user and i use them in route_user.py
+
 in create_new_user i'm collecting all data using UserCreate schema and add it to db
 Also i'm using Hasher class to get the hash of password and add it to db
 """
@@ -21,5 +23,11 @@ def create_new_user(user: UserCreate, db: Session):
     db.commit()
     db.refresh(user)
     return user
+
+
+def show_info(id: int, db: Session):
+    user = db.query(User).filter(User.id == id).first()
+    return user
+
 
 
