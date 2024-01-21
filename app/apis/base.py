1332790d4d605164ff3470eafd25df301e440d39
alexexisted <a.assjones@list.ru>
2024-01-22 01:40:44 +0300

    added router's imports

--- app/apis/base.py
+++ app/apis/base.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter
 
 from apis.v1 import route_user
 from apis.v1 import route_account
+from apis.v1 import route_login
 
 api_router = APIRouter()
 
 api_router.include_router(route_user.router, prefix="", tags=["users"])
 api_router.include_router(route_account.router, prefix="", tags=["accounts"])
+api_router.include_router(route_login.router, prefix="", tags=["login"])
 
 """
 used to import all routes to main.py file
