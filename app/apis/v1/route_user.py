08a77b5b73fc44ef4871db24ced82d3e2bfaaafc
alexexisted <a.assjones@list.ru>
2024-01-21 18:09:21 +0300

    added router to show user's information

--- app/apis/v1/route_user.py
+++ app/apis/v1/route_user.py
@@ -3,12 +3,25 @@ from sqlalchemy.orm import Session
 
 from schemas.user import UserCreate, ShowUser
 from db.session import get_db
-from db.repository.user import create_new_user
+from db.repository.user import create_new_user, show_info
 
 router = APIRouter()
 
+"""
+Here all routers connected to operations with users
+"""
 
-@router.post("/", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
+@router.post("/create-user", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user, db=db)
     return user
+
+
+@router.get("/user/{id}", response_model=ShowUser)
+async def show_user(id: int, db: Session = Depends(get_db)):
+    user = show_info(id=id, db=db)
+    return user
+
+
+
+
