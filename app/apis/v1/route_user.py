eb7e5eb551a4bef53e1ebc0208313156477936bb
alexexisted <a.assjones@list.ru>
2024-01-23 23:42:46 +0300

    removed some imports

--- app/apis/v1/route_user.py
+++ app/apis/v1/route_user.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Request, HTTPException
 from sqlalchemy.orm import Session
+from db.models.user import User
+from jose import JWTError
 
 from schemas.user import UserCreate, ShowUser
 from db.session import get_db
 from db.repository.user import create_new_user, show_info
+from apis.v1.route_login import get_current_user
 
 router = APIRouter()
 
 """
-Here all routers connected to operations with users
+There are all routers connected to operations with users
 """
 
 @router.post("/create-user", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
@@ -21,7 +24,4 @@ async def create_user(user: UserCreate, db: Session = Depends(get_db)):
 async def show_user(id: int, db: Session = Depends(get_db)):
     user = show_info(id=id, db=db)
     return user
-
-
-
 
