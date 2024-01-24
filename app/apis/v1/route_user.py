1aff5173a594b65b704e8a53a3b1f72e0303f7c0
alexexisted <a.assjones@list.ru>
2024-01-24 17:41:57 +0300

    renamed route for registration and renamed route to check user data

--- app/apis/v1/route_user.py
+++ app/apis/v1/route_user.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Depends, status, Request, HTTPException
 from sqlalchemy.orm import Session
 from db.models.user import User
-from jose import JWTError
 
 from schemas.user import UserCreate, ShowUser
 from db.session import get_db
 from db.repository.user import create_new_user, show_info
-from apis.v1.route_login import get_current_user
+from db.repository.login import get_current_user
 
 router = APIRouter()
 
@@ -14,14 +13,16 @@ router = APIRouter()
 There are all routers connected to operations with users
 """
 
-@router.post("/create-user", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
+
+@router.post("/register", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user, db=db)
     return user
 
 
-@router.get("/user/{id}", response_model=ShowUser)
-async def show_user(id: int, db: Session = Depends(get_db)):
-    user = show_info(id=id, db=db)
+@router.get('/profile', response_model=ShowUser)
+async def profile_info(request: Request, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    access_token = request.cookies.get("access_token")
+    if not access_token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return user
-
