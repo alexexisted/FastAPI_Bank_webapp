c551f7196e5821035681d46518147018771239f3
alexexisted <a.assjones@list.ru>
2024-01-24 17:40:30 +0300

    changed routes to make them available only for users who logged in

--- app/apis/v1/route_account.py
+++ app/apis/v1/route_account.py
@@ -1,23 +1,38 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Request, HTTPException
 from sqlalchemy.orm import Session
 
+from db.models.user import User
 from schemas.user import UserCreate, ShowUser
 from db.session import get_db
 from db.repository.user import show_info
 from db.repository.account import deposit, withdraw
+from db.repository.login import get_current_user
 
 router = APIRouter()
 """
 here i made all routes which connected to account operations
 """
 
-@router.post("/deposit", response_model=ShowUser)
-async def deposit_money(id: int, amount: int, db: Session = Depends(get_db)):
-    user = deposit(id=id, amount=amount, db=db)
+
+@router.post("/withdraw", response_model=ShowUser)
+async def withdraw_money(request: Request,
+                         amount: int,
+                         user: User = Depends(get_current_user),
+                         db: Session = Depends(get_db)):
+    access_token = request.cookies.get("access_token")
+    if not access_token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    user = withdraw(id=user.id, amount=amount, db=db)
     return user
 
 
-@router.post("/withdraw", response_model=ShowUser)
-async def withdraw_money(id: int, amount: int, db: Session = Depends(get_db)):
-    user = withdraw(id=id, amount=amount, db=db)
+@router.post('/deposit', response_model=ShowUser)
+async def deposit_money(request: Request,
+                        amount: int,
+                        user: User = Depends(get_current_user),
+                        db: Session = Depends(get_db)):
+    access_token = request.cookies.get("access_token")
+    if not access_token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    user = deposit(id=user.id, amount=amount, db=db)
     return user
